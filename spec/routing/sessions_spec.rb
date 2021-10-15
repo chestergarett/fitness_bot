ec3df4a837d49e975ef143768503e96ea3a39daa
chestergarett <chestergarett@gmail.com>
2021-10-15 19:17:36 +0800

    resolving rubocop tests

--- spec/routing/sessions_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Api::V1::SessionsController do
-  describe 'session routes' do
-    it 'routes to sessions create action' do
-      expect(post('api/v1/sessions')).to route_to({ 'format' => :json, 'controller' => 'api/v1/sessions', 'action' => 'create' })
-    end
-  end
-end
