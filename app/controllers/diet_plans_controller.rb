86aa96c5c3097191bcdd5bccee3159fc06c827c0
visnusantos1203 <visnu.santos1203@gmail.com>
2021-10-17 14:36:12 +0800

    Diet Plan: Created initial views and controller files for diet plan feature to test if diet plans are created according to plan

---
+++ app/controllers/diet_plans_controller.rb
@@ -0,0 +1,54 @@
+class DietPlansController < ApplicationController
+  def index
+    @plans = DietPlan.all
+  end
+
+  def show
+    @plan = DietPlan.find(params[:id])
+  end
+
+  def new
+    @plan = DietPlan.new
+  end
+
+  def create
+    @plan = DietPlan.new(diet_params)
+
+    if @plan.save
+      redirect_to diet_plans_path
+    else
+      # redirect_back fallback_location: root_path # baguhin pa later
+      render :new
+    end
+  end
+
+  def edit
+    @plan = DietPlan.find(params[:id])
+  end
+
+  def update
+    @plan = DietPlan.find(params[:id])
+
+    if @plan.update
+      redirect_to root_path
+    else
+      redirect_back fallback_location: root_path # baguhin pa later
+    end
+  end
+
+  def destroy
+    @plan = DietPlan.find(params[:id])
+    @plan.destroy
+    @plan.save
+
+    if @plan.save
+      render diet_plans_path
+    end
+  end
+
+  private
+
+  def diet_params
+    params.require(:diet_plan).permit(:title, :description, :end_date)
+  end
+end
