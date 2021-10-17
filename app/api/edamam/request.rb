81718d5b96f5afc3988cfe9f80faafee3a48758a
visnusantos1203 <visnu.santos1203@gmail.com>
2021-10-17 14:27:48 +0800

    API Wrapper: Replaced endpoint with params to query on API based on FoodOptions data

--- app/api/edamam/request.rb
+++ app/api/edamam/request.rb
@@ -3,12 +3,12 @@ module Edamam
 
     APPLICATION_ID = "cde8be74"
     APPLICATION_KEY = "6746fb83b091d7a1339f2b70f85ef6fb"
-    BASE_URL = 'https://api.edamam.com/api'
+    BASE_URL = 'https://api.edamam.com/api/recipes/v2'
 
-    def self.call(http_method, endpoint)
+    def self.call(http_method, params)
       result = RestClient::Request.execute(
         method: http_method,
-        url: "#{BASE_URL}#{endpoint}?type=public&app_id=#{APPLICATION_ID}&app_key=#{APPLICATION_KEY}&q=chicken",
+        url: "#{BASE_URL}?type=public&app_id=#{APPLICATION_ID}&app_key=#{APPLICATION_KEY}#{params}",
         headers: {'Accept' => 'application/json'}
       )
 
