f2d8034882a99fac61b5f6faa77ca82fd5bc52a9
visnusantos1203 <visnu.santos1203@gmail.com>
2021-10-17 14:28:44 +0800

    Routes: Added routes for diet plan and food options

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :diet_plans, :food_options
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :sessions, only: [:create, :destroy]
