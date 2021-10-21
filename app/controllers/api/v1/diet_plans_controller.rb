980a2dcc73a655e60b1d1803af997612e0531823
chestergarett <chestergarett@gmail.com>
2021-10-21 11:36:03 +0800

    resolving heroku errors

--- app/controllers/api/v1/diet_plans_controller.rb
+++ app/controllers/api/v1/diet_plans_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class DietPlansController < ApplicationController
-      skip_before_action :authenticate_user!, only: :index
       def index
         plans = DietPlan.all
         render json: plans
