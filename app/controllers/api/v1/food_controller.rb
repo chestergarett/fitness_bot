e04fd55ba851ad36f631e64800fca65ae5fa8fd5
Warda Nur <wardanur04@gmail.com>
2021-10-21 20:20:18 +0800

    rubocop error

--- app/controllers/api/v1/food_controller.rb
+++ app/controllers/api/v1/food_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class FoodController < ApplicationController
-      skip_before_action :authenticate_user_from_token!, only: %i[index create]
+      skip_before_action :authenticate_user_from_token!, only: [:index, :create]
       def index
         food = Food.all
         # food = Food.where(:food_option_id == current_user.food_option_id)
