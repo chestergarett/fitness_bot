7bdf21709f15e2717909ea97f24496b590c51fbb
Warda Nur <wardanur04@gmail.com>
2021-10-22 19:25:22 +0800

    error rubocop

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Food, type: :model do
   context 'with validations' do
-    it 'will not save if title is nil' do
+    it 'will not save if name is nil' do
       plan = described_class.new
-      plan.title = nil
+      plan.name = nil
       plan.description = 'Test description'
       plan.end_date = Time.zone.now
 
@@ -13,7 +13,7 @@ RSpec.describe Food, type: :model do
 
     it 'will not save if description is nil' do
       plan = described_class.new
-      plan.title = 'test title'
+      plan.name = 'test name'
       plan.description = nil
       plan.end_date = Time.zone.now
 
@@ -22,7 +22,7 @@ RSpec.describe Food, type: :model do
 
     it 'will not save if description length is less than 5' do
       plan = described_class.new
-      plan.title = 'test title'
+      plan.name = 'test name'
       plan.description = 'test'
       plan.end_date = Time.zone.now
 
@@ -31,7 +31,7 @@ RSpec.describe Food, type: :model do
 
     it 'will not save if description length is more than 200' do
       plan = described_class.new
-      plan.title = 'test title'
+      plan.name = 'test name'
       plan.description = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
       plan.end_date = Time.zone.now
 
@@ -40,7 +40,7 @@ RSpec.describe Food, type: :model do
 
     it 'will not save if end date is empty' do
       plan = described_class.new
-      plan.title = 'test title'
+      plan.name = 'test name'
       plan.description = 'There are many variations of passages'
       plan.end_date = nil
 
