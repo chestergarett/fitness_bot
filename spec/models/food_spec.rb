7a907c212078352da1fb19e155a597970984e750
Warda Nur <wardanur04@gmail.com>
2021-10-22 19:30:27 +0800

    error rubocop

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -5,44 +5,31 @@ RSpec.describe Food, type: :model do
     it 'will not save if name is nil' do
       plan = described_class.new
       plan.name = nil
-      plan.description = 'Test description'
-      plan.end_date = Time.zone.now
+      plan.ingredients = 'Test ingredients'
 
       expect(plan).not_to be_valid
     end
 
-    it 'will not save if description is nil' do
+    it 'will not save if ingredients is nil' do
       plan = described_class.new
       plan.name = 'test name'
-      plan.description = nil
-      plan.end_date = Time.zone.now
+      plan.ingredients = nil
 
       expect(plan).not_to be_valid
     end
 
-    it 'will not save if description length is less than 5' do
+    it 'will not save if ingredients length is less than 5' do
       plan = described_class.new
       plan.name = 'test name'
-      plan.description = 'test'
-      plan.end_date = Time.zone.now
+      plan.ingredients = 'test'
 
       expect(plan).not_to be_valid
     end
 
-    it 'will not save if description length is more than 200' do
+    it 'will not save if ingredients length is more than 200' do
       plan = described_class.new
       plan.name = 'test name'
-      plan.description = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-      plan.end_date = Time.zone.now
-
-      expect(plan).not_to be_valid
-    end
-
-    it 'will not save if end date is empty' do
-      plan = described_class.new
-      plan.name = 'test name'
-      plan.description = 'There are many variations of passages'
-      plan.end_date = nil
+      plan.ingredients = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
 
       expect(plan).not_to be_valid
     end
