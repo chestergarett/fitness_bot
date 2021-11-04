04151ac44dfe7eddb4c391cf09a95110bf5a8122
visnusantos1203 <visnu.santos1203@gmail.com>
2021-11-04 19:49:19 +0800

    Adds new api method get recommendations for recipes

--- app/api/edamam/client.rb
+++ app/api/edamam/client.rb
@@ -3,5 +3,9 @@ module Edamam
     def self.get_recipes(params)
       Request.call('get', "&q=#{params[:main_ingredient]}&ingr=#{params[:no_of_ingredients]}&diet=#{params[:diet_type]}&health=#{params[:health_label]}&cuisineType=#{params[:cuisine_type]}&mealType=#{params[:meal_type]}&dishType=#{params[:dish_type]}&calories=#{params[:calories]}&excluded=#{params[:excluded]}")
     end
+
+    def self.get_recommendations(diet_type, excluded, meal_type)
+      Request.call('get', "&diet=#{diet_type}&excluded=#{excluded}&mealType=#{meal_type}")
+    end
   end
 end
